9cbfc509d3ca33a754d696ef298833b38ac121e8
mhmdreda99 <Moreda491999@gmail.com>
2021-01-10 01:01:52 +0200

    Edit task_id_009

--- C_Tasks/Task_id_009.c
+++ C_Tasks/Task_id_009.c
@@ -1,7 +1,7 @@
 /**
  * @file Task_id_009.c
  * @author mhmdreda99 (Moreda491999@gmail.com)
- * @brief Program that asks user to enter a key and print its ASCI code 
+ * @brief Program that asks user to enter a key and print its ASCIs code 
  * @version 0.1
  * @date 2021-01-10
  * 
@@ -19,24 +19,28 @@ int main()
 	while(1)
 	{
 		printf("Enter any character:\n\r ");
-		fflush(stdin);fflush(stdout); // clear buffer
+		//read a single character
+		//ch=fgetc(stdin);
 		//read a single character
 		ch=getch();
-		fflush(stdin);fflush(stdout);
+		
+
 		// detect if enter key is pressed
 		if(ch==0x0A)  // ch==10 in decimal 
 		{
+
 			printf("ENTER KEY is pressed.\n");
 			break;
 		}
 		else
 		{
 			printf("( %c )is pressed.\n\r",ch);
-			fflush(stdin);fflush(stdout);
 			printf(" its ASCI : %d.\n",ch);
 		}
 		
 		
+
 	}
 	return 0;
 }
+
